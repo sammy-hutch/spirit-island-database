fd013b5054e675880bd5f88d3086a61cb2a080e3
sammy-hutch <sam-hutch25@hotmail.co.uk>
2025-12-08 23:27:07 +0100

    feat: additional accolade logic

--- dbt/spirit_island_database/models/accolades/spirit_accolades.sql
+++ dbt/spirit_island_database/models/accolades/spirit_accolades.sql
@@ -16,7 +16,8 @@ spirit_game_data_agg AS (
         AVG(game_score) AS avg_game_score
     from spirit_game_data_raw
     group by spirit_name
-)
+),
+mvp AS (
 select
     'MVP' AS accolade_name,
     'highest average score' AS accolade_description,
@@ -25,3 +26,17 @@ from spirit_game_data_agg
 where avg_game_score = (SELECT MAX(avg_game_score) FROM spirit_game_data_agg)
 order by spirit_name
 limit 1
+),
+dq AS (
+select
+    'team playern`t' AS accolade_name,
+    'lowest average score' AS accolade_description,
+    spirit_name
+from spirit_game_data_agg
+where avg_game_score = (SELECT MIN(avg_game_score) FROM spirit_game_data_agg)
+order by spirit_name
+limit 1
+)
+SELECT * from mvp
+union all
+select * from dq
